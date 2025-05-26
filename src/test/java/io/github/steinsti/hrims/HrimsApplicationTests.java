f05d1e6dd7b10f227af17ac44bfc5beeb3431ead
Eston <wantonjack@gmail.com>
2025-05-26 04:01:17 +0300

    Initial commit - Spring Boot HRIMS scaffold

---
+++ src/test/java/io/github/steinsti/hrims/HrimsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.steinsti.hrims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
