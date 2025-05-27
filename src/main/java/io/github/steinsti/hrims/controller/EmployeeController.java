1d3fca618828d34e1c996001a2694316a45de962
Eston <wantonjack@gmail.com>
2025-05-27 23:03:55 +0300

    Adds EmployeeService with CRUD operations
    Introduces EmployeeService to handle employee-related operations.
    Provides methods to retrieve all employees, find employees by ID, save employees, and delete employees by ID.
    
    Improves separation of concerns by delegating database interactions to EmployeeRepository.

---
+++ src/main/java/io/github/steinsti/hrims/controller/EmployeeController.java
@@ -0,0 +1,50 @@
+package io.github.steinsti.hrims.controller;
+
+import io.github.steinsti.hrims.model.Employee;
+import io.github.steinsti.hrims.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.UUID;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @GetMapping
+    public String listEmployees(Model model){
+
+        List<Employee> employees = employeeService.getAllEmployees();
+
+        model.addAttribute("employees", employees);
+
+        return "employees/list";
+    }
+
+    @GetMapping("/add")
+    public String showAddEmployeeForm(Model model) {
+        model.addAttribute("employee", new Employee());
+        return "employee/form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute Employee employee) {
+        employeeService.saveEmployee(employee);
+        return "redirect:/employees";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteEmploye(@PathVariable UUID id) {
+        return "redirect:/employees";
+    }    
+}
---
+++ src/main/java/io/github/steinsti/hrims/service/EmployeeService.java
@@ -0,0 +1,36 @@
+package io.github.steinsti.hrims.service;
+
+import io.github.steinsti.hrims.model.Employee;
+import io.github.steinsti.hrims.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(UUID id) {
+        return employeeRepository.findById(id);
+    }
+    
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(UUID id) {
+        employeeRepository.deleteById(id);
+    }
+}
