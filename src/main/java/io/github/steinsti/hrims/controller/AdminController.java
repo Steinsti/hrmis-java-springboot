2a4230b09b3e3659e43f184cea3e310b635a211d
Eston <wantonjack@gmail.com>
2025-06-05 17:31:13 +0300

    Enhances admin dashboard with dynamic user data and sidebar
    Adds user role and username to the admin dashboard via the model, improving personalization. Updates the sidebar to conditionally display navigation options based on user roles. Refines sidebar responsiveness for mobile and desktop views with Tailwind CSS and JavaScript. Simplifies and modularizes the HTML structure for maintainability.

--- src/main/java/io/github/steinsti/hrims/controller/AdminController.java
+++ src/main/java/io/github/steinsti/hrims/controller/AdminController.java
@@ -1,14 +1,21 @@
 package io.github.steinsti.hrims.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.ui.Model;
 @Controller
 public class AdminController {
 
     @GetMapping("/admin/dashboard")
-    public String adminDashboard(){
+    public String adminDashboard(Model  model){
+    
+        String userRole = SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream()
+            .findFirst().map(Object::toString).orElse("UNKNOWN");
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        model.addAttribute("userRole", userRole);
+        model.addAttribute("userName", userName);
         return "admin/dashboard";
     }
-
 }
